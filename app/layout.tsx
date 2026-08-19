ec030195a276ba48819f90af4a90e9db99919605
dheena-k <dheena.jb@gmail.com>
2026-08-19 16:48:32 +0530

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <head>
-       <link rel="icon" href="/favicon.png" sizes="any" />
+       <link rel="icon" href="/favicon.ico" sizes="any" />
 
       </head>
       <body className="min-h-full flex flex-col">
